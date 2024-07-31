a00fefe802142c15e879684bac3d328d42eeb0f3
scbz <stefan.caminada@ag.ch>
2024-07-31 09:34:54 +0200

    GitLab CI/CD

--- src/blueprints/upload.py
+++ src/blueprints/upload.py
@@ -7,12 +7,12 @@ from werkzeug.utils import secure_filename
 bp = Blueprint("upload", __name__)
 logger = get_module_logger(__name__)
 
-logger.info("Driver, Server, DB, User, Allowed Extensions:")
-#logger.info(current_app.config['DRIVER'])
-#logger.info(current_app.config['SERVER'])
-#logger.info(current_app.config['DB'])
-#logger.info(current_app.config['USER'])
-#logger.info(current_app.config['ALLOWED_EXTENSIONS'])
+logger.debug("Driver, Server, DB, User, Allowed Extensions:")
+logger.debug(current_app.config['DRIVER'])
+logger.debug(current_app.config['SERVER'])
+logger.debug(current_app.config['DB'])
+logger.debug(current_app.config['USER'])
+logger.debug(current_app.config['ALLOWED_EXTENSIONS'])
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in current_app.config['ALLOWED_EXTENSIONS']
@@ -22,21 +22,21 @@ def upload():
     # check if the post request has the file part
     if 'file' not in request.files:
         flash('No file part')
-        logger.info('Request with no file part has been denied.')
+        logger.debug('Request with no file part has been denied.')
         response = {'filename': 'None', 'error': 'No file part'}
         return make_response(jsonify(response), 420)
     file: werkzeug.datastructures.file_storage.FileStorage = request.files['file']
-    logger.info('New file about to be uploaded.')
-    logger.info(file)
-    logger.info(file.content_length)
-    logger.info(file.content_type)
-    logger.info(file.mimetype)
-    logger.info(file.mimetype_params)
+    logger.debug('New file about to be uploaded.')
+    logger.debug(file)
+    logger.debug(file.content_length)
+    logger.debug(file.content_type)
+    logger.debug(file.mimetype)
+    logger.debug(file.mimetype_params)
     # If the user does not select a file, the browser submits an
     # empty file without a filename.
     if file.filename == '':
         flash('No selected file')
-        logger.info('Request with an empty file has been denied.')
+        logger.debug('Request with an empty file has been denied.')
         response = {'filename': 'None', 'error': 'No selected file'}
         return make_response(jsonify(response), 420)
 
@@ -45,27 +45,27 @@ def upload():
 
     # check if the filetype is allowed
     if not allowed_file(filename):
-        logger.info('Request with a wrong file format has been denied.')
+        logger.debug('Request with a wrong file format has been denied.')
         response = {'filename': filename, 'error': 'File format not allowed'}
         return make_response(jsonify(response), 415)
     
     filesize = request.form['file_size']
 
     if int(filesize) > 10000000:
-        logger.info('Request with a file exeeding 10MB has been denied.')
+        logger.debug('Request with a file exeeding 10MB has been denied.')
         response = {'filename': filename, 'error': 'File exeeds 10MB'}
         return make_response(jsonify(response), 413)
     
      # upload the file
-    logger.info('1')
+    logger.debug('1')
     if file:
-        logger.info('2')
+        logger.debug('2')
         try: 
-            logger.info('3')
-            logger.info(current_app.config['DRIVER'])
+            logger.debug('3')
+            logger.debug(current_app.config['DRIVER'])
             connKstaDiversesTest = pyodbc.connect('driver={%s};server=%s;database=%s;uid=%s;pwd=%s;Encrypt=yes;TrustServerCertificate=YES' % ( current_app.config['DRIVER'], current_app.config['SERVER'], current_app.config['DB'], current_app.config['USER'], current_app.config['PASSWORD'] ))
-            logger.info('connection string:')
-            logger.info(connKstaDiversesTest)
+            logger.debug('connection string:')
+            logger.debug(connKstaDiversesTest)
             buffer = BytesIO()
             file.save(buffer)
             bindata  = buffer.getvalue()
@@ -77,7 +77,7 @@ def upload():
             cursor.commit()
             cursor.close()
 
-            logger.info('Upload finished.')
+            logger.debug('Upload finished.')
             response = {"message":"successfully uploaded"}
             return make_response(jsonify(response), 200)
         except Exception as e:
